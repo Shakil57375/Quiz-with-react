d23c2ede29c21fe3d93abd88d85ee0fb3627b341
Shakil57375 <shakil57375@gmail.com>
2023-12-01 12:12:03 +0600

    miniplayer  added

--- src/Components/MiniPlayer/MiniPlayer.jsx
+++ src/Components/MiniPlayer/MiniPlayer.jsx
@@ -1,8 +1,21 @@
-import "./MiniPlayer.css"
+import "./MiniPlayer.css";
+import { FaCirclePlay } from "react-icons/fa6";
+import { IoCloseSharp } from "react-icons/io5";
 const MiniPlayer = () => {
   return (
-    <div>MiniPlayer</div>
-  )
-}
+    <div>
+      <div className="miniPlayer floatingBtn">
+        <span className=" open">
+          <FaCirclePlay />
+        </span>
+        <span className=" close">
+          <IoCloseSharp />
+        </span>
+        <img src="../../../images/3.jpg" alt="" />
+        <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+      </div>
+    </div>
+  );
+};
 
-export default MiniPlayer
+export default MiniPlayer;
