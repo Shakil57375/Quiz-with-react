488462dbd32b556bafb39e16c3522b6f2573df7b
Shakil57375 <shakil57375@gmail.com>
2023-12-01 15:09:44 +0600

    routing setup added

--- src/Components/Analysis/Analysis.jsx
+++ src/Components/Analysis/Analysis.jsx
@@ -5,7 +5,7 @@ const Analysis = () => {
     <div>
       <div className="analysis">
         <h1 className="text-4xl font-bold">Question Analysis</h1>
-        <h4 className="text-lg font-semibold">You answerd 5 out of 10 questions correctly</h4>
+        <h4 className="text-lg font-semibold">You answered 5 out of 10 questions correctly</h4>
         <Answers/>
       </div>
     </div>
--- src/Components/Videos/Videos.jsx
+++ src/Components/Videos/Videos.jsx
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
 import Video from "../Video/Video";
 
 const Videos = () => {
   return (
     <div className="videos grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center gap-5 my-6 mx-auto lg:w-full w-[90%]">
-      <Video />
-      <Video />
-      <Video />
-      <Video />
-      <Video />
-      <Video />
+      <Link to={"/quiz"}><Video /></Link>
+      <Link to={"/quiz"}><Video /></Link>
+      <Link to={"/quiz"}><Video /></Link>
+      <Link to={"/quiz"}><Video /></Link>
+      <Link to={"/quiz"}><Video /></Link>
+      <Link to={"/quiz"}><Video /></Link>
+      <Link to={"/quiz"}><Video /></Link>
+      <Link to={"/quiz"}><Video /></Link>
     </div>
   );
 };
