4c353f88cdb614a20d139b9716e6d034cc64b461
Shakil57375 <shakil57375@gmail.com>
2023-12-04 21:59:50 +0600

    logo added

--- src/Components/Video/Video.jsx
+++ src/Components/Video/Video.jsx
@@ -1,18 +1,45 @@
 import "./Video.css";
-import image from "../../../images/3.jpg"
+import image from "../../../images/3.jpg";
+import { useContext } from "react";
+import { AuthContext } from "../../Context/AuthContext";
+import { useLocation, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+
 const Video = () => {
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const handleLogin = () => {
+    Swal.fire({
+      title: "Please login to select classes",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Login Now!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        navigate("/login", { state: { from: location } });
+      }
+    });
+  };
+
   return (
     <div>
-      <a href="">
-        <div className="video">
-          <img src={image} alt="video title" />
-          <p>#23 React Hooks Bangla </p>
-          <div className="qmeta">
-            <p>10 questions</p>
-            <p>score : not taken yet</p>
+      {user ? (
+        <a href="">
+          <div className="video">
+            <img src={image} alt="video title" />
+            <p>#23 React Hooks Bangla </p>
+            <div className="qmeta">
+              <p>10 questions</p>
+              <p>score : not taken yet</p>
+            </div>
           </div>
-        </div>
-      </a>
+        </a>
+      ) : (
+        handleLogin()
+      )}
     </div>
   );
 };
