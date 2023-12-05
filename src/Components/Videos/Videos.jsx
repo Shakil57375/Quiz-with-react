7d18166fd6f922c40569a52195aba584a82868d6
Shakil57375 <shakil57375@gmail.com>
2023-12-05 14:58:57 +0600

    database  added on firebase

--- src/Components/Videos/Videos.jsx
+++ src/Components/Videos/Videos.jsx
@@ -5,13 +5,6 @@ const Videos = () => {
   return (
     <div className="videos grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center gap-5 my-6 mx-auto lg:w-full w-[90%]">
       <Link to={"/quiz"}><Video /></Link>
-      <Link to={"/quiz"}><Video /></Link>
-      <Link to={"/quiz"}><Video /></Link>
-      <Link to={"/quiz"}><Video /></Link>
-      <Link to={"/quiz"}><Video /></Link>
-      <Link to={"/quiz"}><Video /></Link>
-      <Link to={"/quiz"}><Video /></Link>
-      <Link to={"/quiz"}><Video /></Link>
     </div>
   );
 };
