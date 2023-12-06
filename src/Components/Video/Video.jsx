18e1ac1edabd40583f35b318cd23482aea108e3d
Shakil57375 <shakil57375@gmail.com>
2023-12-06 22:54:53 +0600

    react lazy loader added

--- src/Components/Video/Video.jsx
+++ src/Components/Video/Video.jsx
@@ -1,12 +1,15 @@
+/* eslint-disable react/prop-types */
 import "./Video.css";
-
+import LazyLoad from 'react-lazy-load';
 export default function Video({ title, id, noq }) {
   return (
     <div className="video">
-      <img
-        src={`http://img.youtube.com/vi/${id}/maxresdefault.jpg`}
-        alt={title}
-      />
+      <LazyLoad>
+        <img
+          src={`http://img.youtube.com/vi/${id}/maxresdefault.jpg`}
+          alt={title}
+        />
+      </LazyLoad>
       <p>{title}</p>
       <div className="qmeta">
         <p>{noq} Questions</p>
