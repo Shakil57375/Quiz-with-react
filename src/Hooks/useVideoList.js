5a483a30c37f24562ba0761c0841436feed9a365
Shakil57375 <shakil57375@gmail.com>
2023-12-07 10:07:12 +0600

    page functionality added

--- src/Hooks/useVideoList.js
+++ src/Hooks/useVideoList.js
@@ -1,15 +1,29 @@
+/* eslint-disable no-undef */
 import { useEffect, useState } from "react";
-import { get, getDatabase, orderByKey, query, ref } from "firebase/database";
-const useVideoList = () => {
+import {
+  get,
+  getDatabase,
+  limitToFirst,
+  orderByKey,
+  query,
+  ref,
+  startAt,
+} from "firebase/database";
+const useVideoList = (page) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(true);
-  const [videos, setVideos] = useState([])
+  const [videos, setVideos] = useState([]);
   useEffect(() => {
     async function fetchVideos() {
       // * database related work
       const db = getDatabase();
       const videosRef = ref(db, "videos");
-      const videoQuery = query(videosRef, orderByKey());
+      const videoQuery = query(
+        videosRef,
+        orderByKey(),
+        startAt("" + page),
+        limitToFirst(8)
+      );
       try {
         setError(false);
         setLoading(true);
@@ -17,26 +31,26 @@ const useVideoList = () => {
         const snapshot = await get(videoQuery);
         setLoading(false);
         if (snapshot.exists()) {
-            setVideos((previousVideo)=>{
-                return [...previousVideo, ...Object.values(snapshot.val())]
-            })
+          setVideos((previousVideo) => {
+            return [...previousVideo, ...Object.values(snapshot.val())];
+          });
           /* empty */
         } else {
           //
         }
       } catch (err) {
         console.log(err);
-        setLoading(false)
-        setError(true)
+        setLoading(false);
+        setError(true);
       }
     }
     fetchVideos();
-  }, []);
+  }, [page]);
   return {
     loading,
     error,
-    videos
-  }
+    videos,
+  };
 };
 
 export default useVideoList;
