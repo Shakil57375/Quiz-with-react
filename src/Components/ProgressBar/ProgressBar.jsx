f19ccbed057b21ac4e9d72c7482e1c47ca988319
Shakil57375 <shakil57375@gmail.com>
2023-12-30 12:02:08 +0600

    updated

--- src/Components/ProgressBar/ProgressBar.jsx
+++ src/Components/ProgressBar/ProgressBar.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
 import Button from "../Button/Button";
 import "./ProgressBar.css";
-import { FaArrowRight , FaArrowLeft } from "react-icons/fa";
-const ProgressBar = ({next, prev, submit, progress}) => {
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+const ProgressBar = ({ next, prev, submit, progress }) => {
   return (
     <div className="progressBar">
       <div className="backButton" onClick={prev}>
-        <FaArrowLeft/>
+        <FaArrowLeft />
       </div>
       <div className="rangeArea">
         <div className="tooltip">{progress}% Complete!</div>
@@ -14,10 +14,13 @@ const ProgressBar = ({next, prev, submit, progress}) => {
           <div className="progress" style={{ width: `${progress}%` }}></div>
         </div>
       </div>
-        <Button className="flex items-center px-6 py-2 font-semibold uppercase gap-2 rounded-xl cursor-pointer bg-[#00ff84]" onClick = {progress === 100 ? submit : next}>
-          <span>Next Question</span>
-          <FaArrowRight />
-        </Button>
+      <Button
+        className="flex items-center px-6 py-2 font-semibold uppercase gap-2 rounded-xl cursor-pointer bg-[#00ff84]"
+        onClick={progress === 100 ? submit : next}
+      >
+        <span>{progress === 100 ? "Submit Answers" : "Next Question"}</span>
+        <FaArrowRight />
+      </Button>
     </div>
   );
 };
