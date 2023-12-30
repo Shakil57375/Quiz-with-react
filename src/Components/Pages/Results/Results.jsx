fb683b7444649cc7089747111d67d4cd77bbebdd
Shakil57375 <shakil57375@gmail.com>
2023-12-30 14:37:08 +0600

    updated

--- src/Components/Pages/Results/Results.jsx
+++ src/Components/Pages/Results/Results.jsx
@@ -1,14 +1,19 @@
 import Analysis from "../../Analysis/Analysis";
 import Summary from "../../Summary/Summary";
-import { useParams } from "react-router-dom";
-import useAnswers from "../../Hooks/useAnswers";
+import { useLocation, useParams } from "react-router-dom";
 import "./Results.css";
+import useAnswers from "../../../Hooks/useAnswers";
+import Loader from "../Loader/Loader";
 const Results = () => {
   const { id } = useParams();
-  console.log(id);
+  const { location } = useLocation();
+  const { state } = location;
+  const { qna } = state;
   const { loading, error, answers } = useAnswers(id);
   return (
     <div>
+      {loading && <Loader />}
+      {error && <div>There was an error</div>}
       <Summary />
       <Analysis />
     </div>
